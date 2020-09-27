81b215034b9f98d3db565d23ff709e29ac0195d6
Parapinakes <shiz27@cs.washington.edu>
2020-09-27 15:31:40 -0700

    Initialization

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is the firsty TRY.</Text>
       <StatusBar style="auto" />
     </View>
   );
